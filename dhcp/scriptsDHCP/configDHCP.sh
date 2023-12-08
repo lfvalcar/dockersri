e8b7b6e86bb5beb32863e95cdd08136b21188832
lfvalcar <luisfer998845@gmail.com>
2023-12-08 14:52:50 +0100

    imagen deb-base preparada

--- dhcp/scriptsDHCP/configDHCP.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Script de configuración del servicio DHCP
-
-set -eu # Seguimiento de errores de las variables y carga de las variables de entorno
--- dhcp/start_dhcp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script de inicio del servicio DHCP
-
-set -eu # Seguimiento de errores de las variables y carga de las variables de entorno
-
-# Iniciamos el script de inicio de la imagen base
-bash /root/start.sh
-# Traer funciones
-source /root/scriptsDHCP/configDHCP.sh
