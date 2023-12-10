2bd8460824bcb58836e161ea4b12d61ecdc58d0e
lfvalcar <luisfer998845@gmail.com>
2023-12-10 09:01:36 +0100

    imagen isc-dhcp-server terminada

--- scripts/dhcp/configDHCP.sh
+++ scripts/dhcp/configDHCP.sh
@@ -5,6 +5,22 @@ set -e # Carga de las variables de entorno
 
 configDHCP(){ # Función de configuración del servicio DHCP
     sed -i 's/INTERFACESv4=""/INTERFACESv4="eth0"/g' /etc/default/isc-dhcp-server # Definir las interfaces que participan el servicio DHCP
-    useradd -r -s /usr/sbin/nologin -d /var/run dhcpd
-    chown dhcpd:dhcpd /var/lib/dhcp -R
+    
+    if [ $(grep -c dhcpd /etc/passwd) -eq 1 ]
+    then 
+        echo 'El usuario dhcpd existe'
+        return 1 # Error inesperado
+    else
+        useradd -r -s /usr/sbin/nologin -d /var/run dhcpd # Crear usuario dhcpd
+    fi
+
+    if [ ! -f /var/lib/dhcp/dhcpd.leases ] # Comprobar que existen los archivos de las concesiones
+    then
+        # Crear los archivos de las concesiones
+        touch /var/lib/dhcp/dhcpd.leases
+        touch /var/lib/dhcp/dhcpd.leases~
+    else
+        chown dhcpd:dhcpd /var/lib/dhcp -R # Otorgar la propiedad de los archivos al usuario dhcpd
+        return 0
+    fi
 }
